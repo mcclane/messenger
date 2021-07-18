8c8adc0d03a83dee788060d75a58e994c4d4ef1f
McClane Howland <m@McClanes-MacBook-Pro.local>
2021-07-18 15:54:55 -0700

    do something to it

--- validate_phonenumbers.py
+++ validate_phonenumbers.py
@@ -3,16 +3,20 @@ from csv import DictReader
 import sys
 import pandas as pd
 
+def format_number(mobile, home):
+    n = ''
+    try:
+        n = phonenumbers.parse(mobile, "US")
+    except Exception:
+        try:
+            n = phonenumbers.parse(home, "US")
+        except:
+            return None
+    return phonenumbers.format_number(n, phonenumbers.PhoneNumberFormat.E164)
+
+
 print("PRN,Guid,Language,Phone Number")
 df = pd.read_csv(sys.argv[1])
 for idx, row in df.iterrows():
-    n = ''
-    try:
-        n = phonenumbers.parse(row['mobile'], "US")
-    except Exception:
-        try:
-            n = phonenumbers.parse(row['home'], "US")
-        except:
-            continue
-    n = phonenumbers.format_number(n, phonenumbers.PhoneNumberFormat.E164)
+    n = format_number(row['mobile'], row['home'])
     print("{},{},{},{}".format(row['PRN'], row['guid'], row['language'], n))
